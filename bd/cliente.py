2984d3e79a616b649ef1780642ac1f373f33953c
Manoel <mlimeira@ic.uff.br>
2024-10-09 09:14:41 -0500

    Métodos para listar (get) e cadastrar(insert) clientes

--- bd/cliente.py
+++ bd/cliente.py
@@ -1,4 +1,9 @@
+from tkinter import messagebox
+
 import ttkbootstrap as ttk
+
+from bd.crud import Crud
+
 
 class Tela():
     def __init__(self, master):
@@ -24,12 +29,65 @@ class Tela():
         #Botões
         frm_botoes = ttk.Frame(self.janela)
         frm_botoes.grid(row=1, column=0)
-        btn_cadastrar = ttk.Button(frm_botoes, text='Cadastrar', style='dark')
+        btn_cadastrar = ttk.Button(frm_botoes, text='Cadastrar',
+                                   style='dark', command=self.cadastrar_cliente)
         btn_cadastrar.grid(row=0, column=0, padx=5, pady=5)
         btn_excluir = ttk.Button(frm_botoes, text='Excluir', style='dark')
         btn_excluir.grid(row=0, column=1, padx=5, pady=5)
         btn_editar = ttk.Button(frm_botoes, text='Editar', style='dark')
         btn_editar.grid(row=0, column=2, padx=5, pady=5)
+        self.crud = Crud()
+        self.atualizar_treeview()
+
+    def cadastrar_cliente(self):
+        def confirmar_cadastro():
+            nome = ent_nome.get()
+            cpf = ent_cpf.get()
+            email = ent_email.get()
+            sql = f"""INSERT INTO cliente (nome, cpf, email) 
+                      VALUES ('{nome}', '{cpf}', '{email}');"""
+            if nome == '' or cpf == '' or email == '':
+                messagebox.showwarning('Aviso', 'Todos os campos são '
+                                                'obgrigatórios!',
+                                       parent=self.top_cadastrar)
+            else:
+                if self.crud.insert(sql) == 1:
+                    messagebox.showinfo('Info', 'Cliente inserido com sucesso!')
+                    self.atualizar_treeview()
+                else:
+                    messagebox.showwarning('Aviso', 'Cliente não inserido.')
+                self.top_cadastrar.destroy()
+
+
+        self.top_cadastrar = ttk.Toplevel()
+        self.top_cadastrar.grab_set()
+        lbl_nome = ttk.Label(self.top_cadastrar, text='Nome:')
+        lbl_nome.grid(row=0, column=0)
+        lbl_cpf = ttk.Label(self.top_cadastrar, text='CPF:')
+        lbl_cpf.grid(row=1, column=0)
+        lbl_email = ttk.Label(self.top_cadastrar, text='Email:')
+        lbl_email.grid(row=2, column=0)
+        ent_nome = ttk.Entry(self.top_cadastrar)
+        ent_nome.grid(row=0, column=1)
+        ent_cpf = ttk.Entry(self.top_cadastrar)
+        ent_cpf.grid(row=1, column=1)
+        ent_email = ttk.Entry(self.top_cadastrar)
+        ent_email.grid(row=2, column=1)
+        btn_salvar = ttk.Button(self.top_cadastrar, text='Salvar',
+                                command=confirmar_cadastro)
+        btn_salvar.grid(row=3, column=1)
+
+
+    def atualizar_treeview(self):
+        #Limpa os dados do treeview
+        linhas = self.tvw.get_children()
+        for linha in linhas:
+            self.tvw.delete(linha)
+        #Carrega os dados do banco no treeview
+        sql = "SELECT * FROM cliente;"
+        dados = self.crud.get(sql) #Lista de tuplas
+        for cliente in dados:
+            self.tvw.insert('', 'end', values=cliente)
 
 app = ttk.Window(themename='litera')
 Tela(app)
--- bd/crud.py
+++ bd/crud.py
@@ -5,24 +5,25 @@ class Crud:
         self.conexao = Conexao()
     #inserir
     #conectar, usar a conexao e fechar
-    def insert(self):
-        sql = """INSERT INTO cliente (nome, cpf, email)
-                 VALUES ('Fulano','123','fulano@ufac.br');
-              """
+    def insert(self, sql):
+        #sql = """INSERT INTO cliente (nome, cpf, email)
+        #         VALUES ('Fulano','123','fulano@ufac.br');
+        #      """
         try:
             con = self.conexao.get_conexao()
             cursor = con.cursor()
             cursor.execute(sql)
             if cursor.rowcount == 1:
                 con.commit()
-                print('Registro inserido com sucesso!')
+                #print('Registro inserido com sucesso!')
             con.close()
+            return cursor.rowcount
         except Error as er:
             print(er)
 
     #listar
-    def get(self):
-        sql = """SELECT * FROM cliente;"""
+    def get(self, sql):
+        #sql = """SELECT * FROM cliente;"""
         try:
             con = self.conexao.get_conexao()
             cursor = con.cursor()
@@ -63,10 +64,10 @@ class Crud:
 
 #Teste de conexao
 #teste = Conexao().get_conexao()
-crud = Crud()
+#crud = Crud()
 #crud.insert()
-crud.delete()
-crud.update()
-lista = crud.get()
-for item in lista:
-    print(item)
+#crud.delete()
+#crud.update()
+#lista = crud.get()
+#for item in lista:
+#    print(item)
